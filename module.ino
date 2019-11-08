494cae84aeb94f99b7b79b3438d7f4b61e80f6e7
Victor Moura <victor_cmoura@hotmail.com>
2019-11-08 14:34:15 -0300

    Separates should_run logic from io operation if

--- module.ino
+++ module.ino
@@ -13,30 +13,38 @@ void setup() {
 }
 
 void loop() {
-  if(Serial.available()){
-    int state = Serial.read();
+    print(should_run);
+    print(real_state);
 
-    if(state == 1 && real_state != 1){
-        digitalWrite(dirPin,HIGH);
-        if(real_state == 2){
-            // Should change dir
-            delayMicroseconds(450);
-            digitalWrite(stepPin,LOW);
+    if(Serial.available()){
+        int state = Serial.read();
+
+        if(state == 1 && real_state != 1){
+            digitalWrite(dirPin,HIGH);
+            
+            if(real_state == 2){
+                // Should change dir
+                delayMicroseconds(450);
+                digitalWrite(stepPin,LOW);
+            }
+
+            should_run = 1;
+            real_state = 1;
+        }else if(state == 2 && real_state != 2){
+            digitalWrite(dirPin,LOW);
+
+            if(real_state == 1){
+                // Should change dir
+                delayMicroseconds(450);
+                digitalWrite(stepPin,LOW);
+            }
+            
+            should_run = 1;
+            real_state = 2;
+        }else{
+            should_run = 0;
+            real_state = state;
         }
-        should_run = 1;
-        real_state = 1;
-    }else if(state == 2 && real_state != 2){
-        digitalWrite(dirPin,LOW);
-        if(real_state == 1){
-            // Should change dir
-            delayMicroseconds(450);
-            digitalWrite(stepPin,LOW);
-        }
-        should_run = 1;
-        real_state = 2;
-    }else{
-        should_run = 0;
-        real_state = state;
     }
 
     if(should_run){
@@ -44,6 +52,5 @@ void loop() {
         digitalWrite(stepPin,HIGH); 
         delayMicroseconds(450);
         digitalWrite(stepPin,LOW);
-    } 
-  }
+    }
 }
